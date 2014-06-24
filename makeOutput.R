27acf2bdb5c2f30be072902bf4739d99ed872f90
rockclimber112358 <rockclimber112358@gmail.com>
2014-06-24 11:28:53 -0600

    Update makeOutput.R

--- makeOutput.R
+++ makeOutput.R
@@ -7,10 +7,20 @@ makeOutput = function(preds, call)
     stop("is.matrix(preds) | is.data.frame(preds).  Ensure preds is a numeric vector!")
   if(!"Submissions" %in% list.files() )
     stop("No submissions directory!")
-  if(nrow(d)!=17379)
+  if(nrow(d)!=17379 & nrow(d)!=17379*2)
     stop("d should have 17379 rows!")
-  if( length(preds)!=17379 & length(preds)!=6493 )
-    stop("preds should have 17379 or 6493 values!")
+  if( length(preds)!=17379 & length(preds)!=6493 & length(preds)!=17379*2 )
+    stop("preds should have 17379, 6493, or 17379*2 values!")
+
+  if(nrow(d)==17379){
+    actual = d$count
+    datetime = d$datetime
+  }
+  if(nrow(d)==17379*2){
+    actual = d$count[d$countType=="registered"]
+    datetime = d$datetime[d$countType=="registered"]
+    preds = preds[1:17379] + preds[17380:(2*17379)]
+  }
 
   files = list.files("Submissions")
   files = files[grepl("_raw", files)]
@@ -19,14 +29,14 @@ makeOutput = function(preds, call)
   newId = max(ids,0)+1
   
   write.csv(preds, paste0("Submissions/",newId,"_raw.csv"), row.names=F)
-  if(length(preds)==17379){
-    #Do Cross-validation since you have all predictions
-    score = mean( (log(preds+1)-log(d$count+1))^2, na.rm=T )
-    write.csv( data.frame(datetime=d$datetime[is.na(d$count)], count=preds[is.na(d$count)] )
-          ,file=paste0(newId,"_",score,".csv"), row.names=F)
+  if(length(preds)==6493){
+    write.csv( data.frame(datetime=datetime[is.na(d$count)], count=preds )
+          ,file=paste0("Submissions/",newId,"_NA.csv"), row.names=F)    
   } else {
-    write.csv( data.frame(datetime=d$datetime[is.na(d$count)], count=preds )
-          ,file=paste0(newId,"_NA.csv"), row.names=F)    
+    #Do Cross-validation since you have all predictions
+    score = sqrt( mean( (log(preds+1)-log(actual+1))^2, na.rm=T ) )
+    write.csv( data.frame(datetime=datetime[is.na(actual)], count=preds[is.na(actual)] )
+          ,file=paste0("Submissions/",newId,"_",score,".csv"), row.names=F)
   }
   if("desc.csv" %in% list.files("Submissions") ){
     desc = read.csv("Submissions/desc.csv", stringsAsFactors=F)
@@ -34,5 +44,5 @@ makeOutput = function(preds, call)
   } else {
     desc = data.frame(newId, call)
   }
-  write.csv(desc, file="Submissions/desc.csv")
+  write.csv(desc, file="Submissions/desc.csv", row.names=F)
 }
